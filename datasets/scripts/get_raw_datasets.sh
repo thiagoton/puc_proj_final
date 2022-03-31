889505b7234f3483e1fe9b1b6ae1add01896d1c1
Thiago Ton <thiago@pumatronix.com.br>
2022-03-31 10:46:46 -0300

    fix rclone raw data fetch

--- datasets/scripts/get_raw_datasets.sh
+++ datasets/scripts/get_raw_datasets.sh
@@ -2,10 +2,10 @@
 
 if [[ -z $PUC_PROJ_ENV_SET ]]; then
     echo "Environment is not configured set"
-    exit -1
+    exit 255
 fi
 
 RCLONE_REMOTE=puc_data_bucket
-REMOTE_PATH=/files/raw_files
+REMOTE_PATH=files/raw_files
 
-rclone sync --progress $RCLONE_REMOTE:$REMOTE_PATH/* $PROJ_ROOT/datasets/samples/raw/
+rclone sync --progress $RCLONE_REMOTE:$REMOTE_PATH/ $PROJ_ROOT/datasets/samples/raw/
